b843bd785453e3355a40e3d0858558d285e2bd15
D.Naydenov <d.naydenov@futurist-labs.com>
2019-06-16 21:49:50 +0300

    implemented an aws handler to incoke the parsing

---
+++ index.js
@@ -0,0 +1,15 @@
+const parser = require('./parser');
+const commands = require('./commands');
+
+exports.handler = async (event) => {
+    const code = event.code;
+    const input = Array.from(event.input).map(symbol => Number(symbol));
+    try {
+        const codeCommands = parser.parse(code);
+        const result = commands.execute(codeCommands, input);
+        return JSON.stringify(result);
+    } catch (error) {
+        const errorResponse = { code: error.code, message: error.message };
+        return JSON.stringify(errorResponse);
+    }
+};
